e4fb0ff7d8c830968d96b6f6563747a2ce5d8edb
AndreaTB08 <andreat.atb@gmail.com>
2025-11-01 17:47:51 -0600

    Implementación endpoint PUT /book/:id por Andrea Trochez

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ app.post('/book', (req,res)=>{
     res.json({status:200, message:'success', data:book});
 });
 
+app.put('/book/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = books.findIndex(b => b.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ mensaje: 'Libro no encontrado' });
+    }
+
+    const { titulo, autor, genero, anioPublicacion } = req.body;
+
+    books[index] = {
+        ...books[index],
+        titulo: titulo || books[index].titulo,
+        autor: autor || books[index].autor,
+        genero: genero || books[index].genero,
+        anioPublicacion: anioPublicacion || books[index].anioPublicacion
+    };
+
+    res.json({
+        mensaje: 'Libro actualizado correctamente',
+        libro: books[index]
+    });
+});
 app.listen(PORT, ()=>{
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
